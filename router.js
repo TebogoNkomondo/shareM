ab6b0e34702cf0c0a160c5294f6a9e3803f5a6b4
TebogoNkomondo <1124458@students.wits.ac.za>
2020-07-12 23:30:11 +0200

    added controller for homepage

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('./controllers/userController')
+const followController = require('./controllers/followController')
 
-router.get('/', (req, res) => {
-  res.render('home-guest')
-})
+router.get('/', userController.home)
 
 module.exports = router
