834094d54708e86712baa00d1b856673f2de35b5
TebogoNkomondo <1124458@students.wits.ac.za>
2020-07-13 14:52:51 +0200

    db connection environment variables

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const mongodb = require('mongodb')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const connectionString = 'mongodb+srv://toDoAppUser:todoappuser@cluster0.fcwxa.mongodb.net/shareM?retryWrites=true&w=majority'
-
-mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
   if (err) {
     console.log('error connecting to db')
   } else {
     module.exports = client.db()
     const app = require('./app')
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log('listening on port 3000')
     })
   }
