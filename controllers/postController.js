6295f2aaf86a9648219d48fb3e1bd04a428596c8
TebogoNkomondo <1124458@students.wits.ac.za>
2020-07-14 20:46:48 +0200

    basic post creation

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,14 @@
+const Post = require('../models/Post')
+
 exports.viewCreateScreen = (req, res) => {
   res.render('create-post')
+}
+
+exports.create = (req, res) => {
+  const post = new Post(req.body)
+  post.create().then(function () {
+    res.send('new post created')
+  }).catch(function (errors) {
+    res.send(errors)
+  })
 }
--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const userController = require('./controllers/userController')
 const followController = require('./controllers/followController')
 const postController = require('./controllers/postController')
+const app = require('./app')
 
 // user related routes
 router.get('/', userController.home)
@@ -11,5 +12,6 @@ router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 // post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 
 module.exports = router
