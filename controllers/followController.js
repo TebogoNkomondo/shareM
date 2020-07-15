1ee889a1decc940ed2e3c3ca26e8a77ce655aa16
TebogoNkomondo <1124458@students.wits.ac.za>
2020-07-15 18:23:46 +0200

    diplaying following users

--- controllers/followController.js
+++ controllers/followController.js
@@ -4,15 +4,24 @@ exports.addFollow = function (req, res) {
   const follow = new Follow(req.params.username, req.visitorId)
   follow.create().then(() => {
     req.flash('success', `Successfully followed ${req.params.username}`)
-    req.session.save(() => {
-      res.redirect(`/profile/${req.params.username}`)
-    })
+    req.session.save(() => res.redirect(`/profile/${req.params.username}`))
   }).catch((errors) => {
-    errors.forEach((error) => {
+    errors.forEach(error => {
       req.flash('errors', error)
     })
-    req.session.save(() => {
-      res.redirect('/')
+    req.session.save(() => res.redirect('/'))
+  })
+}
+
+exports.removeFollow = function (req, res) {
+  const follow = new Follow(req.params.username, req.visitorId)
+  follow.delete().then(() => {
+    req.flash('success', `Successfully stopped following ${req.params.username}`)
+    req.session.save(() => res.redirect(`/profile/${req.params.username}`))
+  }).catch((errors) => {
+    errors.forEach(error => {
+      req.flash('errors', error)
     })
+    req.session.save(() => res.redirect('/'))
   })
 }
