95f14bd811fdbb13fcf80faa690f758a6c0e1288
TebogoNkomondo <1124458@students.wits.ac.za>
2020-07-15 17:52:15 +0200

    user can follow each other

---
+++ controllers/followController.js
@@ -0,0 +1,18 @@
+const Follow = require('../models/Follow')
+
+exports.addFollow = function (req, res) {
+  const follow = new Follow(req.params.username, req.visitorId)
+  follow.create().then(() => {
+    req.flash('success', `Successfully followed ${req.params.username}`)
+    req.session.save(() => {
+      res.redirect(`/profile/${req.params.username}`)
+    })
+  }).catch((errors) => {
+    errors.forEach((error) => {
+      req.flash('errors', error)
+    })
+    req.session.save(() => {
+      res.redirect('/')
+    })
+  })
+}
