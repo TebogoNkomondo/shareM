99f77e60cc7faea4b59396b07a2d7f0844b707cc
TebogoNkomondo <1124458@students.wits.ac.za>
2020-07-16 13:05:25 +0200

    dynamic port number

--- db.js
+++ db.js
@@ -2,13 +2,18 @@ const mongodb = require('mongodb')
 const dotenv = require('dotenv')
 dotenv.config()
 
+let port = process.env.PORT
+if (port == null || '') {
+  port = process.env.PORT
+}
+
 mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
   if (err) {
     console.log('error connecting to db')
   } else {
     module.exports = client
     const app = require('./app')
-    app.listen(process.env.PORT, () => {
+    app.listen(port, () => {
       console.log('listening on port 3000')
     })
   }
